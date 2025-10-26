b52868308951123084df338b414c1728ae3192d8
Letyzynha <lety.martins05@gmail.com>
2025-10-25 21:05:56 -0300

    Add custom User model and register in admin

--- AteliMatch/user/admin.py
+++ AteliMatch/user/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as DefaultUserAdmin
+from .models import User
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(DefaultUserAdmin):
+	model = User
+	# Use the default UserAdmin configuration; customize list_display if desired
+	list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff')
--- AteliMatch/user/models.py
+++ AteliMatch/user/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+	"""Modelo de usuário customizado.
+
+	Nesse momento herdamos de AbstractUser sem campos adicionais.
+	Você pode estender adicionando campos (por exemplo: phone, is_artist, profile_picture).
+	"""
+
+	# Exemplo de campo adicional (comente/descomente se quiser usar)
+	# phone = models.CharField(max_length=20, blank=True, null=True)
+
+	def __str__(self) -> str:
+		return self.username
